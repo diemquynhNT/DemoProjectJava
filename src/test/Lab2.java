bc21b58515a3f094be0c6cdee842b50ea4077bea
penguin <115617334+diemquynhNT@users.noreply.github.com>
2023-10-09 15:52:17 +0700

    DQ

--- src/test/Lab2.java
+++ src/test/Lab2.java
@@ -10,6 +10,7 @@ package test;
  */
 public class Lab2 {
     public static void main(String[] args){
-    
+        System.err.println("hfhsdgfhjgdfhadfhsdfhasgdfashdgf");
+        //kdshfjksdhfkjhsdjhfkhadfhkjshdkjfkjdhffkjsdkjfhkjasdfkjhaskjdhfkj
     }
 }
